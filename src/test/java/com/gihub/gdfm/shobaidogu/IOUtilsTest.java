0923c8103f6300737beb8b7cfcc34aa4de76ea2b
Gianmarco De Francisci Morales <gdfm+github@gdfm.me>
2013-12-16 12:03:46 +0100

    Added Apache license, changed group id/package, switched to new development version

---
+++ src/test/java/com/gihub/gdfm/shobaidogu/IOUtilsTest.java
@@ -0,0 +1,41 @@
+package com.gihub.gdfm.shobaidogu;
+
+/*
+ * #%L
+ * shobai-dogu
+ * %%
+ * Copyright (C) 2012 - 2013 gdfm
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.junit.Test;
+
+public class IOUtilsTest {
+
+    @Test
+    public void testGetBufferedReader() {
+        // fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReadStopwords() {
+        // fail("Not yet implemented");
+    }
+
+    @Test
+    public void testReadIdfs() {
+        // fail("Not yet implemented");
+    }
+}
