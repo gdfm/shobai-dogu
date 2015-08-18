b298a66ff8c2a0bbaa671c768ef9df22da856136
Gianmarco De Francisci Morales <gdfm@apache.org>
2015-08-18 15:42:17 +0300

    Add reporting period to ProgressTracker

--- src/main/java/com/github/gdfm/shobaidogu/ProgressTracker.java
+++ src/main/java/com/github/gdfm/shobaidogu/ProgressTracker.java
@@ -29,40 +29,42 @@ import org.slf4j.LoggerFactory;
  * Tracks the progress of your long running computation. Tracking is printed on a class Logger at INFO level.
  */
 public class ProgressTracker {
-    private static final Logger LOG = LoggerFactory.getLogger(ProgressTracker.class);
-    private long tick;
-    private int count, prevCount, totalIterations;
+  private static final Logger LOG = LoggerFactory.getLogger(ProgressTracker.class);
+  private long tick;
+  private int count, prevCount, totalIterations;
+  private int period;
 
-    /**
-     * Builds a progress tracker that gives an estimate time of arrival for the computation, given the current speed.
-     * 
-     * @param estimatedIterations
-     *            the expected number of iterations to run.
-     */
-    public ProgressTracker(int estimatedIterations) {
-        tick = System.currentTimeMillis();
-        count = prevCount = 0;
-        totalIterations = estimatedIterations;
-    }
+  /**
+   * Builds a progress tracker that gives an estimate time of arrival for the computation, given the current speed.
+   * 
+   * @param estimatedIterations
+   *          the expected number of iterations to run.
+   */
+  public ProgressTracker(int estimatedIterations, int reportPeriod) {
+    tick = System.currentTimeMillis();
+    count = prevCount = 0;
+    totalIterations = estimatedIterations;
+    period = reportPeriod;
+  }
 
-    /**
-     * Logs the progress.
-     */
-    public void progress() {
-        count++;
-        if (count % 1000 == 0 && LOG.isInfoEnabled()) {
-            final double percent = 100 * (count / (double) totalIterations);
-            final long tock = System.currentTimeMillis();
-            final long timeInterval = tock - tick;
-            final long linesPerSec = (count - prevCount) * 1000 / timeInterval;
-            tick = tock;
-            prevCount = count;
-            final int etaSeconds = (int) ((totalIterations - count) / linesPerSec);
-            final long hours = SECONDS.toHours(etaSeconds);
-            final long minutes = SECONDS.toMinutes(etaSeconds - HOURS.toSeconds(hours));
-            final long seconds = SECONDS.toSeconds(etaSeconds - MINUTES.toSeconds(minutes));
-            LOG.info(String.format("[%3.0f%%] Completed %d iterations of %d total input. %d iters/s. ETA %02d:%02d:%02d", percent, count, totalIterations,
-                    linesPerSec, hours, minutes, seconds));
-        }
+  /**
+   * Logs the progress.
+   */
+  public void progress() {
+    count++;
+    if (count % period == 0 && LOG.isInfoEnabled()) {
+      final double percent = 100 * (count / (double) totalIterations);
+      final long tock = System.currentTimeMillis();
+      final long timeInterval = tock - tick;
+      final long linesPerSec = (count - prevCount) * 1000 / timeInterval;
+      tick = tock;
+      prevCount = count;
+      final int etaSeconds = (int) ((totalIterations - count) / linesPerSec);
+      final long hours = SECONDS.toHours(etaSeconds);
+      final long minutes = SECONDS.toMinutes(etaSeconds - HOURS.toSeconds(hours));
+      final long seconds = SECONDS.toSeconds(etaSeconds - MINUTES.toSeconds(minutes));
+      LOG.info(String.format("[%3.0f%%] Completed %d iterations of %d total input. %d iters/s. ETA %02d:%02d:%02d",
+          percent, count, totalIterations, linesPerSec, hours, minutes, seconds));
     }
+  }
 }
